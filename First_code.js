18e60d1842264c01c9a695642ae8d99f29a51fea
Shivam <shivams334@gmail.com>
2015-09-20 13:16:40 +0530

    This is my first js code

---
+++ First_code.js
@@ -0,0 +1,6 @@
+var feedback=prompt("Rate us")
+if (feedback>8){
+    console.log("Thank you! We should race at the next concert!");
+}else{
+    console.log("I'll keep practicing coding and racing.");
+}
